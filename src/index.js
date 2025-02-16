d0edeb0313375faf026eaf71202644eeb5568b8d
Tuan Anh <tp.anhh7@gmail.com>
2025-02-16 23:54:00 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import GameSavingLoader from "../js/GameSavingLoader";
 
 // Задание 1
-// GameSavingLoader.load()
-// 	.then(saving => {
-// 		console.log("Данные загружены:", saving);
-// 	})
-// 	.catch(error => {
-// 		console.error("Ошибка:", error);
-// 	});
-
-// Задание 2
-(async () => {
-	try {
-		const saving = await GameSavingLoader.load();
+GameSavingLoader.load()
+	.then(saving => {
 		console.log("Данные загружены:", saving);
-	} catch (error) {
+	})
+	.catch(error => {
 		console.error("Ошибка:", error);
-	}
-})();
+	});
+
+// // Задание 2
+// (async () => {
+// 	try {
+// 		const saving = await GameSavingLoader.load();
+// 		console.log("Данные загружены:", saving);
+// 	} catch (error) {
+// 		console.error("Ошибка:", error);
+// 	}
+// })();
